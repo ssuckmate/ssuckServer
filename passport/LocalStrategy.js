730abdf49b3a633fe5e309502ee078c3d2d8637a
BraveSejin <ihavebravebiz@gmail.com>
2020-11-10 16:57:26 +0900

    로그인안돼살려줘

--- passport/LocalStrategy.js
+++ passport/LocalStrategy.js
@@ -1,7 +1,6 @@
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
-
-var bcrypt = require('bcrypt')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt')
 const { User } = require('../models');
 
 module.exports = () => {
@@ -9,17 +8,24 @@ module.exports = () => {
     passport.use(new LocalStrategy({
         usernameField: 'email',
         passwordField: 'password'
-    },
-
-    async (email, password, done) => {
-        var user = await User.findOne({
-            where: {
-                email: email,
-                password: password
-            }
-        });
-        if (!user) done(null, false);
-        else done(null, user);
+    },async (email, password, done) => {
+        log("tlqkf")
+        try{
+            const user = await User.findOne({
+                where: {
+                    email: email,
+                    password: password
+                }
+            });
+            console.log(user);
+            if (!user) done(null, false, {message:'이메일이나 비밀번호가 일치하지 않습니다.'});
+            else {
+                done(null, user);  
+            }  
+        }catch(error){
+            done(error);
+        }
+        
     }));
 
     return passport;
--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -9,20 +9,34 @@ const secret = process.env.JWT || '13@@4d%sf!a'
 
 const router = express.Router()
 
-router.post('/login', passport.authenticate('local', {
-    session: false
-    }), (req, res) => {
-        const token = jwt.sign({
-            email: req.user.email,
-            password: req.user.password
-        }, secret);
-        console.log(token);
-        res.status(200).send(token);
-    }
+router.post('/login', async (req,res,next) =>{
+    console.log("askldfjn");
+    passport.authenticate('local', {session:false},(authError, user, info) =>{
+        if(authError){
+            console.error(authError);
+            return res.status(500).json('알 수 없는 에러입니다.');
+        }
+        if(!user){
+            return res.status(202).json(info.message);
+        }
+        // const token = jwt.sign({req.user.email}, secret);
+        return res.status(200).json(token);
+    })    
+}
 );
+
+    // passport.authenticate('local', {session: false}, ()),
+    // (req, res) => {
+    //     const token = jwt.sign({
+    //         email: req.user.email,
+    //     }, secret);
+    //     console.log(token);
+    //     res.status(200).send(token);
+    // }
 
 router.post('/join', async (req, res, next) => {
     const {email, password, name} = req.body;
+    console.log('tl;qkf');
     try{
         const exUser = await User.findOne({where:{email}});
         if(exUser){
