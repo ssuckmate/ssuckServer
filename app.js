9967fec3f21d5e89ee22f3785b13a2ce8a64dc01
BraveSejin <ihavebravebiz@gmail.com>
2020-12-21 16:55:13 +0900

    V1 폐기, V3 추가

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const passport = require('passport');
 const jsyaml = require('js-yaml');
 const fs = require('fs');
 const swaggerUI = require('swagger-ui-express');
-const swaggerV1 = fs.readFileSync('swaggerV1.yaml','utf8')
+const swaggerV3 = fs.readFileSync('swaggerV3.yaml','utf8')
 const swaggerV2 = fs.readFileSync('swaggerV2.yaml','utf8')
-const swaggerV1Doc = jsyaml.safeLoad(swaggerV1);
+const swaggerV3Doc = jsyaml.safeLoad(swaggerV3);
 const swaggerV2Doc = jsyaml.safeLoad(swaggerV2);
 
 const app = express();
@@ -32,7 +32,7 @@ passportConfig(); // passport 사용
 
 const routes = require('./routes');
 
-app.use('/api-docs/v1',swaggerUI.serve,swaggerUI.setup(swaggerV1Doc));
+app.use('/api-docs/v3',swaggerUI.serve,swaggerUI.setup(swaggerV3Doc));
 app.use('/api-docs/v2',swaggerUI.serve,swaggerUI.setup(swaggerV2Doc));
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const sagamV1 = require('./v1/sagam/index');
-const userV1 = require('./v1/user/index')
-
-router.use('/v1/sagam', sagamV1);
-router.use('/v1/user', userV1);
-
 const sagamV2 = require('./v2/sagam/index');
 const userV2 = require('./v2/user/index')
-
 router.use('/v2/sagam', sagamV2);
 router.use('/v2/user', userV2);
+
+const sagamV3 = require('./v3/sagam/index');
+const userV3 = require('./v3/user/index')
+router.use('/v3/sagam', sagamV3);
+router.use('/v3/user', userV3);
 
 module.exports = router;
