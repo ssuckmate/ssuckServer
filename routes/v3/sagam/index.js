4a04170f5a38eebde1934f8a586bda71dce23d15
BraveSejin <ihavebravebiz@gmail.com>
2020-12-27 15:03:16 +0900

    /sagam/room  get, post

--- routes/v3/sagam/index.js
+++ routes/v3/sagam/index.js
@@ -6,11 +6,13 @@ const dormitory = require('./dormitory');
 const parcel = require('./parcel');
 const user = require('./user');
 const washer = require('./washer')
+const room = require('./room')
 
 router.use('/auth', auth)
 router.use('/dormitory', dormitory)
 router.use('/parcel', parcel)
 router.use('/user', user);
 router.use('/washer', washer);
+router.use('/room',room)
 
 module.exports = router;
