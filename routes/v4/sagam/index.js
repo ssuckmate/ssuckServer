91751339eadb1251195178e3bc3d432bca417d53
BraveSejin <ihavebravebiz@gmail.com>
2021-01-18 23:45:01 +0900

    사감, 유저가 자기 정보 얻을 수 있게 함

--- routes/v4/sagam/index.js
+++ routes/v4/sagam/index.js
@@ -6,7 +6,10 @@ const dormitory = require('./dormitory');
 const parcel = require('./parcel');
 const user = require('./user');
 const washer = require('./washer')
-const room = require('./room')
+const room = require('./room');
+const { hasToken } = require('../middlewares');
+const { Sagam } = require('../../../models');
+
 
 router.use('/auth', auth)
 router.use('/dormitory', dormitory)
@@ -15,4 +18,17 @@ router.use('/user', user);
 router.use('/washer', washer);
 router.use('/room',room)
 
+router.get('/',hasToken ,async (req, res, next) => {
+   
+    try{
+        const sagam = await Sagam.findOne({where:{
+            id: req.decode.id
+        }})
+       
+        return res.status(201).json(sagam )
+    }catch(error){
+        console.error(error);
+        return next(error);
+    }
+} )
 module.exports = router;
--- routes/v4/user/index.js
+++ routes/v4/user/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { User } = require('../../../models');
+const { hasToken } = require('../middlewares');
 const router = express.Router();
 
 const auth = require('./auth');
@@ -7,5 +9,19 @@ const washer = require('./washer');
 router.use('/auth', auth);
 router.use('/parcel', parcel);
 router.use('/washer', washer);
+
+router.get('/',hasToken ,async (req, res, next) => {
+   
+    try{
+        const user = await User.findOne({where:{
+            id: req.decode.id
+        }})
+       
+        return res.status(201).json(user)
+    }catch(error){
+        console.error(error);
+        return next(error);
+    }
+} )
 
 module.exports = router;
