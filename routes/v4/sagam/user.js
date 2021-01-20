0d4e6e96fe0df4905368aad5e78542df0ddb6b7f
BraveSejin <ihavebravebiz@gmail.com>
2021-01-20 10:44:33 +0900

    userInfo

--- routes/v4/sagam/user.js
+++ routes/v4/sagam/user.js
@@ -4,7 +4,19 @@ const {hasToken} = require('../middlewares');
 
 const router = express.Router()
 
-
+router.get('/userInfo', hasToken,async(req, res, next) =>{
+    try{
+        const user = await User.findOne({
+            where:{
+                id: req.query.user
+            } 
+        });
+        return res.status(200).json(user);
+    }catch(error){
+        console.error(error);
+        return next(error);
+    }
+})
 router.get('/list', hasToken,async(req, res, next) =>{
     try{
 
