fd3077bfc99c7bac0a495dd149ad806f9bde7f19
BraveSejin <ihavebravebiz@gmail.com>
2021-01-20 14:58:34 +0900

    공지사항 api에 노출

--- routes/v4/sagam/notice.js
+++ routes/v4/sagam/notice.js
@@ -1,8 +1,28 @@
 const express = require('express');
-const { Sagam, Notice, Dormitory } = require('../../../models');
+const { Sagam, Notice } = require('../../../models');
 const {hasToken} = require('../middlewares');
 
 const router = express.Router()
+
+
+router.get('/', hasToken, async (req, res, next) =>{
+    try{
+        const sagam = await Sagam.findOne({
+            where:{
+                id:req.decode.id
+            }
+        })
+        const notices = await Notice.findAll({
+            where: {
+                dormitory: sagam.dormitory,
+            }
+        })
+        return res.status(200).json(notices);
+    }catch(error){
+        console.error(error);
+        return next(error);
+    }
+})
 
 router.post('/add', hasToken, async (req, res, next) =>{
     try{
@@ -23,7 +43,7 @@ router.post('/add', hasToken, async (req, res, next) =>{
 
 router.delete('/delete', hasToken, async (req, res, next) =>{
     try{
-        await Notice.destroy({where:{id: req.body.notice}});
+        await Notice.destroy({where:{id: req.query.notice}});
         return res.status(200).json({
             message: `공지가 삭제되었습니다.`
         });
